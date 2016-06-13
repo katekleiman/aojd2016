e16bb962f3e3fae8d81a710a9dd62f5666d836a2
Kate Kleiman <kate@heritagemuse.com>
2016-06-13 13:31:15 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://use.typekit.net/xtv8tlz.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<title>AOJD Home</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link href="css/nav-side-menu.css" rel="stylesheet" type="text/css">
+<link href="tng/templates/template6/css/templatestyle.css" rel="stylesheet" type="text/css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<?php include("header.php"); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-9">
+      <h2>Welcome to AOJD-Online.net<br>
+      </h2>
+      <p>Building on he work of Rabbi Stern, <b>AOJD-online.net</b> travels well beyond the original concept of Rabbi Stern's books.  Stern originally focused on Jewish families in the U. S. and Canada prior to 1830. He most often worked from direct family correspondence, synagogue records, early cemetery records and only occasionally from government published data and other public records. </p>
+      
+      <p>The new AOJD database makes Rabbi Stern's work both more accessible (full search capabilities on all data fields) . The new database also reflects the 50+ pages of corrections and additions suggested by Rabbi Stern in the 2<sup>nd</sup> and 3<sup>rd</sup> editions.  New corrections and missed families and individuals then extend the available information.</p><p>Lastly, the new information set goes further by beginning the task of tracking numerous families that arrived between 1830 and 1870. In particular the links between major mercantile and political families become evident as the population of the U. S. grows and we approach the era of mass Eastern European immigration.</p>
+
+<p>The AOJD-online database is being carefully researched to current genealogical standards to prove much of the family information published in the earlier work.  New sources include U.S. and local government records from vital records (birth, marriage, death, divorce, probate, deeds, etc.) to census, ship arrivals, immigration and naturalization, and military records as well as local historical newspapers. In additional to drawing from commercial online databases, on-site research has been conducted from Montreal, Canada throughout the U. S. and into the Caribbean Islands where many of these families originated.  Small steps in bridging the genealogical gap between the colonial U. S. and both Great Britain and the Netherlands have already begun to enter into this data collection.</p>
+
+
+<p>As Rabbi Stern discovered, many of the families whose lineage is represented herein are enthusiastic supporters of this project.  They continue to share updates and modifications (documentation is required) most generously and several new lines have been added.  In particular, we want to acknowledge the sharing and extend our great thanks to Anne Josephs (Montreal, Canada), Jonathan de Sola-Mendes (New York City), Ainsley Henriques (Kingston, Jamaica), Louis Kessler (Mikve Israel Archives, Philadelphia), and many others.</p>
+
+Proofing, corrections, and new data entry are an ongoing effort of Kate Kleiman and the staff at Heritage Muse, Inc.  The on-line database is updated quarterly. </div>
+<div class="row">
+<div class="col-lg-3">
+
+<h3>FAQ</h3>
+<div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab">
+      <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1">Search Tips</a></h4>
+    </div>
+    <div id="collapseOne1" class="panel-collapse collapse in">
+      <div class="panel-body">The search feature on this site allows wildcard searches.</div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo1">GEDCOM Downloads</a></h4>
+    </div>
+    <div id="collapseTwo1" class="panel-collapse collapse">
+      <div class="panel-body">We have decided not to allow GEDCOM downloads to individual subscribers. As genealogists, we know that you would love to sidestep all the data entry work and put this research directly into your private genealogy database. At some point in the future, we may offer upgraded subscription levels that would allow GEDCOM downloads, so stay tuned. </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseThree1">Updates to Information</a></h4>
+    </div>
+    <div id="collapseThree1" class="panel-collapse collapse">
+      <div class="panel-body">The genealogy data is updated with the latest research twice a year. New content, like pictures, documents, newspaper articles and other resources are added frequently. Check back if you don't find what you are seeking in your initial search.</div>
+    </div>
+  </div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseFour1">Copyright and Fair Use</a></h4>
+    </div>
+    <div id="collapseFour1" class="panel-collapse collapse">
+      <div class="panel-body">We understand that you may want to use the genealogy data, pictures and documents for your private research. The images belonging to Heritage Muse may be downloaded for personal use and are marked with the Heritage Muse watermark.  However, if you publish your research on a website or in a family history book, you need to contact us to obtain permission to use any images you find here. 
+Some of the images on the site are copyrighted by other institutions. Heritage Muse has paid for the right to include these images on the site, and if you want to use them in an online or print publication, you must do the same. When in doubt, ask by filling out the "Rights & Reproductions" form.</div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+  <?php include ("footer.php"); ?>
+  
+  
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+
+
